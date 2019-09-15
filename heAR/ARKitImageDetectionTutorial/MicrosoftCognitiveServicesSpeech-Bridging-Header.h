e5b5a93820855592a36a3766b93d8589e450b9d6
Pranav Panchal <panchaldpranav@gmail.com>
2019-09-15 06:31:29 -0400

    Add heAR again

---
+++ heAR/ARKitImageDetectionTutorial/MicrosoftCognitiveServicesSpeech-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  MicrosoftCognitiveServicesSpeech-Bridging-Header.h
+//  ARKitImageDetectionTutorial
+//
+//  Created by Pranav Panchal on 2019-09-15.
+//  Copyright © 2019 Ivan Nesterenko. All rights reserved.
+//
+
+#ifndef MicrosoftCognitiveServicesSpeech_Bridging_Header_h
+#define MicrosoftCognitiveServicesSpeech_Bridging_Header_h
+
+#import <MicrosoftCognitiveServicesSpeech/SPXSpeechAPI.h>
+
+#endif /* MicrosoftCognitiveServicesSpeech_Bridging_Header_h */
